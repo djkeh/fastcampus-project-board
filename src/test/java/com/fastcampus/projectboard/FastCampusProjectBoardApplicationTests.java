245b4f14b1276ba6928bdb52fc7bbcfcb515f51c
Uno Kim <djkehh@gmail.com>
2022-06-08 01:26:56 +0900

    #6 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기 기술은:
    
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/test/java/com/fastcampus/projectboard/FastCampusProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCampusProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
