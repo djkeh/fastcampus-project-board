a6c3ebad2e7f5dadf75fd1fc36b834fbd949b15d
Uno Kim <djkehh@gmail.com>
2022-06-15 05:28:29 +0900

    #16 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트까지 한 번에 적용

--- src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.fastcampus.projectboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -17,4 +18,13 @@ public class ArticleController {
 
         return "articles/index";
     }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map) {
+        map.addAttribute("article", "article"); // TODO: 구현할 때 여기에 실제 데이터를 넣어줘야 한다
+        map.addAttribute("articleComments", List.of());
+
+        return "articles/detail";
+    }
+
 }
--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -34,7 +34,6 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
