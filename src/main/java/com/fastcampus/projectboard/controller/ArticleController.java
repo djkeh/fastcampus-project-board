bcfa3fe964b4e4e122ecdecddbb4cd05cdb76096
Uno Kim <djkehh@gmail.com>
2022-06-15 04:38:00 +0900

    #16 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메소드로 테스트를 통과하게 열어줌
    뷰 적절히 뼈대 잡음
    테스트에서는 media type 검사에서 부가적인 옵션이
    들어갈 수 있음을 확인했기 때문에
    적절히 테스트 메소드 수정

--- src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -1,9 +1,20 @@
 package com.fastcampus.projectboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+    @GetMapping
+    public String articles(ModelMap map) {
+        map.addAttribute("articles", List.of());
+
+        return "articles/index";
+    }
 }
--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -21,7 +21,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -30,7 +29,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
