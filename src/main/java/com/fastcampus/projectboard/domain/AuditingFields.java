0f7e865634a4a83ec2a002786bb54fcd7b701259
Uno Kim <djkehh@gmail.com>
2022-06-15 01:35:21 +0900

    리팩토링: AuditingFields 클래스를 추상 클래스로 변경
    엔티티에서 상속을 해서 사용해야 하는 목적에 더 잘 맞게
    `abstract` 키워드 추가

--- src/main/java/com/fastcampus/projectboard/domain/AuditingFields.java
+++ src/main/java/com/fastcampus/projectboard/domain/AuditingFields.java
@@ -18,7 +18,7 @@ import java.time.LocalDateTime;
 @ToString
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
-public class AuditingFields {
+public abstract class AuditingFields {
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @CreatedDate
