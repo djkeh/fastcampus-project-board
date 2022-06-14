b781e26cf404c738cfb5f67eaabab5bb364be248
Uno Kim <djkehh@gmail.com>
2022-06-14 19:14:30 +0900

    #17 - 리팩토링: 사용 안하는 import 정리
    강의 중 optimize import 하지 않은 듯.
    정리함

--- src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
