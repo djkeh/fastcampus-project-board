d45c1ef1951533efd0f2bb8f99e7966ee7246b85
Uno Kim <djkehh@gmail.com>
2022-06-21 00:35:32 +0900

    DTO 들에 Serializable 인터페이스 제거
    JPA Buddy 를 이용해 작성한 DTO들인데,
    자동으로 `implements Serializable` 이 들어가버림
    우리 프로젝트는 직렬화로 Jackson을 사용하므로
    필요하지 않고, 의도하여 넣은 코드도 아님
    그러므로 삭제

--- src/main/java/com/fastcampus/projectboard/dto/response/ArticleCommentResponse.java
+++ src/main/java/com/fastcampus/projectboard/dto/response/ArticleCommentResponse.java
@@ -2,7 +2,6 @@ package com.fastcampus.projectboard.dto.response;
 
 import com.fastcampus.projectboard.dto.ArticleCommentDto;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 
 public record ArticleCommentResponse(
@@ -11,7 +10,7 @@ public record ArticleCommentResponse(
         LocalDateTime createdAt,
         String email,
         String nickname
-) implements Serializable {
+) {
 
     public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname) {
         return new ArticleCommentResponse(id, content, createdAt, email, nickname);
--- src/main/java/com/fastcampus/projectboard/dto/response/ArticleWithCommentsResponse.java
+++ src/main/java/com/fastcampus/projectboard/dto/response/ArticleWithCommentsResponse.java
@@ -2,7 +2,6 @@ package com.fastcampus.projectboard.dto.response;
 
 import com.fastcampus.projectboard.dto.ArticleWithCommentsDto;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -17,7 +16,7 @@ public record ArticleWithCommentsResponse(
         String email,
         String nickname,
         Set<ArticleCommentResponse> articleCommentsResponse
-) implements Serializable {
+) {
 
     public static ArticleWithCommentsResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentResponse> articleCommentResponses) {
         return new ArticleWithCommentsResponse(id, title, content, hashtag, createdAt, email, nickname, articleCommentResponses);
