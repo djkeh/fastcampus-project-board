5c14da77283eb75ea508c811826d30916546b60a
Uno Kim <djkehh@gmail.com>
2022-06-23 02:20:19 +0900

    #42 - Spring Data REST 추가 설정 - userId 노출
    data rest 기본 설정은 id를 감추는 것인데,
    회원 계정에 한해서 `userId`가 노출되게끔 해줘야 함

---
+++ src/main/java/com/fastcampus/projectboard/config/DataRestConfig.java
@@ -0,0 +1,18 @@
+package com.fastcampus.projectboard.config;
+
+import com.fastcampus.projectboard.domain.UserAccount;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class DataRestConfig {
+
+    @Bean
+    public RepositoryRestConfigurer repositoryRestConfigurer() {
+        return RepositoryRestConfigurer.withConfig((config, cors) ->
+                config.exposeIdsFor(UserAccount.class)
+        );
+    }
+
+}
