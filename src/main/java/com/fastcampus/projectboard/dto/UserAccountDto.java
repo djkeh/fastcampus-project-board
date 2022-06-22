d8ccb5f047b266274fff4f62fffa7dc60419073d
Uno Kim <djkehh@gmail.com>
2022-06-23 02:15:23 +0900

    #42 - 회원 계정 도메인 변경사항을 영향받는 코드에 적용
    `id` -> `userId`로 id를 변경하는 내용을
    모든 자바코드에 반영

--- src/main/java/com/fastcampus/projectboard/dto/UserAccountDto.java
+++ src/main/java/com/fastcampus/projectboard/dto/UserAccountDto.java
@@ -5,7 +5,6 @@ import com.fastcampus.projectboard.domain.UserAccount;
 import java.time.LocalDateTime;
 
 public record UserAccountDto(
-        Long id,
         String userId,
         String userPassword,
         String email,
@@ -16,13 +15,12 @@ public record UserAccountDto(
         LocalDateTime modifiedAt,
         String modifiedBy
 ) {
-    public static UserAccountDto of(Long id, String userId, String userPassword, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
-        return new UserAccountDto(id, userId, userPassword, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
+    public static UserAccountDto of(String userId, String userPassword, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new UserAccountDto(userId, userPassword, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
     }
 
     public static UserAccountDto from(UserAccount entity) {
         return new UserAccountDto(
-                entity.getId(),
                 entity.getUserId(),
                 entity.getUserPassword(),
                 entity.getEmail(),
--- src/main/java/com/fastcampus/projectboard/repository/UserAccountRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/UserAccountRepository.java
@@ -3,5 +3,5 @@ package com.fastcampus.projectboard.repository;
 import com.fastcampus.projectboard.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
 }
