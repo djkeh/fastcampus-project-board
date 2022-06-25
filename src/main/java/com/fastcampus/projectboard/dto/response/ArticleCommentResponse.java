aa919393f0a31a27c18732930de98b474ca87fa1
Uno Kim <djkehh@gmail.com>
2022-06-25 10:13:43 +0900

    #34 - 수정, 삭제를 할 때 해당 사용자가 맞는지 확인하기 위한 응답 정보 추가
    인증 정보의 `userId`와
    해당 게시글/댓글의 작성자 `userId`가 동일한지 알려면
    응답에서 `userId`를 내려줘야 함
    이를 response dto 에 반영

--- src/main/java/com/fastcampus/projectboard/dto/response/ArticleCommentResponse.java
+++ src/main/java/com/fastcampus/projectboard/dto/response/ArticleCommentResponse.java
@@ -9,11 +9,12 @@ public record ArticleCommentResponse(
         String content,
         LocalDateTime createdAt,
         String email,
-        String nickname
+        String nickname,
+        String userId
 ) {
 
-    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname) {
-        return new ArticleCommentResponse(id, content, createdAt, email, nickname);
+    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId) {
+        return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId);
     }
 
     public static ArticleCommentResponse from(ArticleCommentDto dto) {
@@ -27,7 +28,8 @@ public record ArticleCommentResponse(
                 dto.content(),
                 dto.createdAt(),
                 dto.userAccountDto().email(),
-                nickname
+                nickname,
+                dto.userAccountDto().userId()
         );
     }
 
