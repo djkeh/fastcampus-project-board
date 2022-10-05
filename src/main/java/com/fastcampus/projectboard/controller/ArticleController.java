e62a48448cb07ccea50479adbe138b1934ad0811
Uno Kim <djkehh@gmail.com>
2022-10-06 08:46:29 +0900

    #70 - 게시글 컨트롤러의 해시태그 기능 변경점 반영
    이 정보는 뷰에서 해시태그에 링크를 달 때
    게시글의 검색 필터 정보로 쓰기 위해 필요하다.
    
    * TODO: `searchTypes`에서 재활용할 수 있는 데이터이므로 더 나은 방법이 있을지 고민해보자.
    
    테스트의 변화는 #68 의 e9f36a4 에서 도메인 변경을 하면서
    이미 구상하여 작업하였음

--- src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -41,6 +41,7 @@ public class ArticleController {
         map.addAttribute("articles", articles);
         map.addAttribute("paginationBarNumbers", barNumbers);
         map.addAttribute("searchTypes", SearchType.values());
+        map.addAttribute("searchTypeHashtag", SearchType.HASHTAG);
 
         return "articles/index";
     }
@@ -52,6 +53,7 @@ public class ArticleController {
         map.addAttribute("article", article);
         map.addAttribute("articleComments", article.articleCommentsResponse());
         map.addAttribute("totalCount", articleService.getArticleCount());
+        map.addAttribute("searchTypeHashtag", SearchType.HASHTAG);
 
         return "articles/detail";
     }
