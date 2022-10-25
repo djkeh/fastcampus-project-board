3b8af7c1ef86a634326d5624eb2c9aba9ace98b1
Uno Kim <djkehh@gmail.com>
2022-10-26 08:17:02 +0900

    #101 - 댓글 컨트롤러 테스트 업데이트: 대댓글 케이스 추가
    대댓글이 포함된 댓글 저장 api 의 입출력 테스트 추가

--- src/test/java/com/fastcampus/projectboard/controller/ArticleCommentControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleCommentControllerTest.java
@@ -91,4 +91,26 @@ class ArticleCommentControllerTest {
         then(articleCommentService).should().deleteArticleComment(articleCommentId, userId);
     }
 
+    @WithUserDetails(value = "unoTest", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("[view][POST] 대댓글 등록 - 정상 호출")
+    @Test
+    void givenArticleCommentInfoWithParentCommentId_whenRequesting_thenSavesNewChildComment() throws Exception {
+        // Given
+        long articleId = 1L;
+        ArticleCommentRequest request = ArticleCommentRequest.of(articleId, 1L, "test comment");
+        willDoNothing().given(articleCommentService).saveArticleComment(any(ArticleCommentDto.class));
+
+        // When & Then
+        mvc.perform(
+                        post("/comments/new")
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .content(formDataEncoder.encode(request))
+                                .with(csrf())
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/articles/" + articleId))
+                .andExpect(redirectedUrl("/articles/" + articleId));
+        then(articleCommentService).should().saveArticleComment(any(ArticleCommentDto.class));
+    }
+
 }
