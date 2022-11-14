747c71aece47f60fd92816c4d2db0ed1fb172ef5
Uno Kim <djkehh@gmail.com>
2022-11-15 01:04:21 +0900

    #120 - 메인 스프링 부트 테스트에서 발생하는 에러 해결
    DB 접근 정보가 환경변수 문법으로 치환되었는데
    `@SpringBootTest`가 이를 그대로 읽으면서
    올바른 jdbc url 포맷이 아니므로 실패를 유발함
    
    이에 적당히 인메모리 테스트 db인 h2 경로를
    새로 만든 테스트 전용 `test` 프로파일에 지정해주고,
    테스트 실행 시 이 프로파일을 바라보게 하여
    문제 해결

--- src/test/java/com/fastcampus/projectboard/FastCampusProjectBoardApplicationTests.java
+++ src/test/java/com/fastcampus/projectboard/FastCampusProjectBoardApplicationTests.java
@@ -2,7 +2,9 @@ package com.fastcampus.projectboard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class FastCampusProjectBoardApplicationTests {
 
