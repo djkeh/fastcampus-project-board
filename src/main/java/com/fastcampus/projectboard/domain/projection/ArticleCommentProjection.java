0968c9609587e94ee491b24e950935cb3b239019
Uno Kim <djkehh@gmail.com>
2023-03-15 04:03:46 +0900

    #128 - UserAccount api 노출 추가 + data rest projection 을 이용한 데이터 노출 필드 설정
    `UserAccount`를 노출할 api로 추가해 줌.
    이렇게 하면 다른 게시글, 댓글 등의 data rest api 노출 데이터 형식이
    원하지 않는 스타일로 바뀌게 된다.
    이를 수동으로 설정하기 위해 projection 기능을 사용하여
    노출할 필드들을 직접 지정함.

---
+++ src/main/java/com/fastcampus/projectboard/domain/projection/ArticleCommentProjection.java
@@ -0,0 +1,19 @@
+package com.fastcampus.projectboard.domain.projection;
+
+import com.fastcampus.projectboard.domain.ArticleComment;
+import com.fastcampus.projectboard.domain.UserAccount;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.time.LocalDateTime;
+
+@Projection(name = "withUserAccount", types = ArticleComment.class)
+public interface ArticleCommentProjection {
+    Long getId();
+    UserAccount getUserAccount();
+    Long getParentCommentId();
+    String getContent();
+    LocalDateTime getCreatedAt();
+    String getCreatedBy();
+    LocalDateTime getModifiedAt();
+    String getModifiedBy();
+}
--- src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,7 @@ package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.domain.Article;
 import com.fastcampus.projectboard.domain.QArticle;
+import com.fastcampus.projectboard.domain.projection.ArticleProjection;
 import com.fastcampus.projectboard.repository.querydsl.ArticleRepositoryCustom;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
@@ -13,7 +14,7 @@ import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(excerptProjection = ArticleProjection.class)
 public interface ArticleRepository extends
         JpaRepository<Article, Long>,
         ArticleRepositoryCustom,
