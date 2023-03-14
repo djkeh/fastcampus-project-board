0756b4bc731e7ff9945b6f5b24c39f70a5644367
Uno Kim <djkehh@gmail.com>
2023-03-15 03:53:16 +0900

    #126 - api 서비스들의 시큐리티 설정 업데이트
    외부 서비스인 어드민 프로젝트에서 원활하게
    게시판 서비스의 api를 이용할 수 있도록
    보안 설정을 수정.
    
    * csrf 인증을 api 통신일 때만 비활성화
    * api 요청은 인증 없이 가능

--- src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
         return http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .mvcMatchers("/api/**").permitAll()
                         .mvcMatchers(
                                 HttpMethod.GET,
                                 "/",
@@ -48,6 +49,7 @@ public class SecurityConfig {
                                 .userService(oAuth2UserService)
                         )
                 )
+                .csrf(csrf -> csrf.ignoringAntMatchers("/api/**"))
                 .build();
     }
 
