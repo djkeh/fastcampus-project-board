37dbe61f3756c2fd4bb79910685ac5385cacae40
Uno Kim <djkehh@gmail.com>
2023-03-15 04:12:15 +0900

    #129 - 새 계정 이메일 오타 수정
    2가 아니라 3.
    이메일은 unique해야 하기 때문에,
    기존 계정 이메일과 동일한 오타를 쓰면 안됨.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,7 +7,7 @@ insert into user_account (user_id, user_password, nickname, email, memo, created
 ('uno2', '{noop}asdf1234', 'Uno2', 'uno2@mail.com', 'I am Uno2.', now(), 'uno2', now(), 'uno2')
 ;
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-('uno3', '{noop}asdf1234', 'Uno3', 'uno2@mail.com', 'I am Uno3.', now(), 'uno3', now(), 'uno3')
+('uno3', '{noop}asdf1234', 'Uno3', 'uno3@mail.com', 'I am Uno3.', now(), 'uno3', now(), 'uno3')
 ;
 
 -- 123 게시글
